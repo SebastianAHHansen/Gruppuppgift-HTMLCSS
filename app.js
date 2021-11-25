721feb08ecce1aafadec23ce22a46e2cc28c8cfd
SebastianAHHansen <91529190+SebastianAHHansen@users.noreply.github.com>
2021-11-25 13:10:22 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -3,3 +3,19 @@
 let btnHeart = document.querySelector('#heart');
 btnHeart.addEventListener('click', () => btnHeart.style.color = 'red');
 
+//Script Search function
+const listJackets = document.getElementById('listJackets');
+const searchBar = document.getElementById('searchBar');
+let allJackets = [];
+
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+    const filteredJackets = allJackets.filter((jackets) => {
+        return{
+            jackets.jacketName.toLowerCase().includes(searchString) || jackets.jacketDefinition.toLowerCase().includes(searchString) || jackets.material.toLowerCase().includes(searchString)
+        };
+    });
+    displayJackets(filteredJackets);
+});
+
